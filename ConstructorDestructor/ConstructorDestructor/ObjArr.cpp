a3d61d295d4ca75d2f00c0f5c6e15ae4cddd71dc
godjhs0727 <godjhs0727@gmail.com>
2021-04-01 02:57:05 +0900

    202104010257

---
+++ ConstructorDestructor/ConstructorDestructor/ObjArr.cpp
@@ -0,0 +1,62 @@
+//객체배열 알아보기
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person
+{
+private:
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage)
+	{
+		name = new char[strlen(myname) + 1];
+		strcpy(name, myname);
+		age = myage;
+	}
+	Person() //배열 생성시 나오는 default 생성자
+	{
+		name = NULL;
+		age = 0;
+		cout << "called Person()" << endl;
+	}
+	void SetPersonInfo(const char* myname, int myage) //원하는 데이터로의 초기화를 목적으로 정의된 함수이다. 생략해도 무방
+	{
+		name = new char[strlen(myname) + 1];
+		strcpy(name, myname);
+		age = myage;
+	}
+	void ShowPersonInfo() const
+	{
+		cout << "이름: " << name << ", ";
+		cout << "나이: " << age << endl;
+	}
+	~Person()
+	{
+		delete[] name;
+		cout << "called destructor" << endl;
+	}
+};
+
+int main()
+{
+	Person *parr[3];
+	char namestr[100];
+	char* strptr;
+	int age;
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "이름: ";
+		cin >> namestr;
+		cout << "나이: ";
+		cin >> age;
+		parr[i] = new Person(namestr, age);
+	}
+	parr[0]->ShowPersonInfo();
+	parr[1]->ShowPersonInfo();
+	parr[2]->ShowPersonInfo();
+
+	return 0;
+}
