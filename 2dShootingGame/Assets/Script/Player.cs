46d6e5e1a62dfb2e22c1ac34a97f109f52ada465
godjhs0727 <godjhs0727@gmail.com>
2021-06-11 15:09:01 +0900

    202106111508

--- 2dShootingGame/Assets/Script/Player.cs
+++ 2dShootingGame/Assets/Script/Player.cs
@@ -4,23 +4,90 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchRight;
     public bool isTouchLeft;
+
+    public float speed;
+    public float power;
+    public float maxShotDelay;
+    public float curShotDelay;
+
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();   
+    }
     void Update()
     {
+        Move();
+        Fire();
+        Reroad();
+    }
+    void Move()
+    {
         float h = Input.GetAxisRaw("Horizontal");
-        if((h == 1 && isTouchTop) || (h == -1 && isTouchBottom))
+        if ((h == 1 && isTouchRight) || (h == -1 && isTouchLeft))
             h = 0;
         float v = Input.GetAxisRaw("Vertical");
-        if ((v == 1 && isTouchRight) || (v == -1 && isTouchLeft))
+        if ((v == 1 && isTouchTop) || (v == -1 && isTouchBottom))
             v = 0;
         Vector3 curPos = transform.position;
         Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime; //transform 이동에는 Time.DeltaTime꼭 사용하기
 
         transform.position = curPos + nextPos;
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1"))
+            return;
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        switch (power)
+        {
+            case 1:
+                //Power One
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation); //Destroy와 반대, 생성
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation); //Destroy와 반대, 생성
+                GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.35f, transform.rotation); //Destroy와 반대, 생성
+                GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.35f, transform.rotation);
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+
+        }
+        curShotDelay = 0;
+    }
+    void Reroad()
+    {
+        curShotDelay += Time.deltaTime; //딜레이 변수에 Time.deltaTime을 계속 더하여 시간계산
+
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
