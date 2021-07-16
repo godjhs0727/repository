c8dab4d278f27359266cf1ce302fecc558b206b5
godjhs0727 <godjhs0727@gmail.com>
2021-07-17 04:25:40 +0900

    202107170425

--- 3d/Assets/Script/Weapon.cs
+++ 3d/Assets/Script/Weapon.cs
@@ -8,8 +8,16 @@ public class Weapon : MonoBehaviour
     public Type type;
     public int damage;
     public float rate;
+    public int maxAmmo;
+    public int curAmmo;
+
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
+
 
     public void Use()
     {
@@ -17,6 +25,12 @@ public class Weapon : MonoBehaviour
         {
             StopCoroutine("Swing");
             StartCoroutine("Swing");
+        }
+        else if(type == Type.Range && curAmmo> 0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");
+
         }
     }
     IEnumerator Swing() //코루틴 Invoke대용
@@ -31,6 +45,21 @@ public class Weapon : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
         trailEffect.enabled = false;
+    }
+    IEnumerator Shot()
+    {
+        //#1.총알 발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+
+        yield return null;
+        //#2.탄피배출
+        GameObject instantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = instantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec, ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10, ForceMode.Impulse);
     }
 
     //Use() 메인루틴 -> Swing() 서브루틴 -> Use() 메인루틴
